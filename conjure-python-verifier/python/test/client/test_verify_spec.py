58a36dd7f48a0cec6e971759744e2c20b74e544b
Dan Sanduleac <dsanduleac@palantir.com>
2018-07-19 14:06:15 +0100

    Assert blacklisted tests would actually fail

--- conjure-python-verifier/python/test/client/test_verify_spec.py
+++ conjure-python-verifier/python/test/client/test_verify_spec.py
@@ -37,6 +37,18 @@ def generate_param_tests(test_kind):
             for i, value in enumerate(test_kinds)]
 
 
+def run_test(should_pass, is_blacklisted, runnable):
+    # If the test is blacklisted, we inverse the logic, to ensure that it would have "failed" the normal test.
+    if should_pass ^ is_blacklisted:
+        runnable()
+    else:
+        with pytest.raises(Exception):
+            runnable()
+    # If it did behave as intended, then skip it in the end if it was blacklisted.
+    if is_blacklisted:
+        pytest.skip("Blacklisted")
+
+
 @pytest.mark.parametrize('endpoint_name,method_name,index,case,should_pass', generate_auto_deserialize_tests())
 def test_body(
         conjure_validation_server,
@@ -49,36 +61,34 @@ def test_body(
         case,
         should_pass):
     body_black_list = test_black_list['autoDeserialize']
-    if endpoint_name in body_black_list and case in body_black_list[endpoint_name]:
-        pytest.skip("Blacklisted")
+    is_blacklisted = endpoint_name in body_black_list and case in body_black_list[endpoint_name]
 
     if should_pass:
-        return confirm_service.confirm(endpoint_name, index, getattr(body_service, method_name)(index))
+        run_test(True,
+                 is_blacklisted,
+                 lambda: confirm_service.confirm(endpoint_name, index, getattr(body_service, method_name)(index)))
     else:
-        with pytest.raises(Exception):
-            getattr(body_service, method_name)(index)
+        run_test(False, is_blacklisted, lambda: getattr(body_service, method_name)(index))
 
 
 @pytest.mark.parametrize('endpoint_name,method_name,index,value', generate_param_tests('singleHeaderService'))
 def test_header(conjure_validation_server, test_black_list, header_service, endpoint_name, method_name, index, value):
     header_black_list = test_black_list['singleHeaderService']
-    if endpoint_name in header_black_list and value in header_black_list[endpoint_name]:
-        pytest.skip("Blacklisted")
+    is_blacklisted = endpoint_name in header_black_list and value in header_black_list[endpoint_name]
 
-    return getattr(header_service, method_name)(index, json.loads(value))
+    run_test(True, is_blacklisted, lambda: getattr(header_service, method_name)(index, json.loads(value)))
 
 
 @pytest.mark.parametrize('endpoint_name,method_name,index,value', generate_param_tests('singlePathParamService'))
 def test_path(conjure_validation_server, test_black_list, path_service, endpoint_name, method_name, index, value):
     header_black_list = test_black_list['singlePathParamService']
-    if True or endpoint_name in header_black_list and value in header_black_list[endpoint_name]:
-        pytest.skip("Blacklisted")
-    return getattr(path_service, method_name)(index, json.loads(value))
+    is_blacklisted = endpoint_name in header_black_list and value in header_black_list[endpoint_name]
+
+    run_test(True, is_blacklisted, lambda: getattr(path_service, method_name)(index, json.loads(value)))
 
 
 @pytest.mark.parametrize('endpoint_name,method_name,index,value', generate_param_tests('singleQueryParamService'))
 def test_query(conjure_validation_server, test_black_list, query_service, endpoint_name, method_name, index, value):
     query_black_list = test_black_list['singleQueryService']
-    if endpoint_name in query_black_list and value in query_black_list[endpoint_name]:
-        pytest.skip("Blacklisted")
-    return getattr(query_service, method_name)(index, json.loads(value))
+    is_blacklisted = endpoint_name in query_black_list and value in query_black_list[endpoint_name]
+    run_test(True, is_blacklisted, lambda: getattr(query_service, method_name)(index, json.loads(value)))
