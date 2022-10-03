0bbc64052fab5320ec644f8ce1f32a97928595c3
Paul Nepywoda <pnepywoda@palantir.com>
2022-10-03 10:34:52 -0700

    Require only python 3.6

--- conjure-python-core/src/main/java/com/palantir/conjure/python/ConjurePythonGenerator.java
+++ conjure-python-core/src/main/java/com/palantir/conjure/python/ConjurePythonGenerator.java
@@ -252,7 +252,7 @@ public final class ConjurePythonGenerator {
                 .pythonPackage(rootPackage)
                 .putOptions("name", config.packageName().get())
                 .putOptions("version", config.packageVersion().get())
-                .putOptions("python_requires", ">=3.8")
+                .putOptions("python_requires", ">=3.6")
                 .putRawOptions("package_data", "{\"\": [\"py.typed\"]}")
                 .addInstallDependencies("requests")
                 .addInstallDependencies(String.format(
