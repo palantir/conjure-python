46ae88dbe6593c46345f8707de3551e2d36d72cc
Andrew Floren <afloren@palantir.com>
2023-08-09 14:09:35 -0400

    remove

--- conjure-python-verifier/python/test/client/test_import_all.py
+++ conjure-python-verifier/python/test/client/test_import_all.py
@@ -1,6 +1,4 @@
 from generated_integration import *
-from generated_integration.product import *
-from totally_fake_made_up import *
 
 def test():
     pass  # we just need to confirm that the above import didn't fail
