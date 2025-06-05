aa8f8507c8e2f6b18e15b7d0fe956c54490839f6
AlexandreGuinaudeau <aguinaudeau@palantir.com>
2025-06-05 17:26:47 +0200

    PR review

--- conjure-python-core/src/main/java/com/palantir/conjure/python/poet/UnionSnippet.java
+++ conjure-python-core/src/main/java/com/palantir/conjure/python/poet/UnionSnippet.java
@@ -200,7 +200,7 @@ public interface UnionSnippet extends PythonSnippet {
                     "raise ValueError('{} is not an instance of %s'.format(visitor.__class__.__name__))", visitorName);
             poetWriter.decreaseIndent();
             options().forEach(option -> {
-                if (parameterName(option).equals("optional")) {
+                if (!option.isOptional()) {
                     poetWriter.writeIndentedLine("if self._type == '%s':", parameterName(option));
                 } else {
                     poetWriter.writeIndentedLine(
